5b3d3bad0c834bd1118415f9e240b4c5237aeb44
Dmitriy Khmaladze <dmitriy@itadapter.com>
2018-01-14 18:52:18 -0500

    Amend Restore;Rebuild build script targets

--- build-release.cmd
+++ build-release.cmd
@@ -10,7 +10,8 @@ set NFX_HOME=%PROJECT_HOME%NFXv5\
 set DOTNET_FRAMEWORK_DIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
 
 set MSBUILD_EXE="%DOTNET_FRAMEWORK_DIR%\MSBuild.exe"
-set BUILD_ARGS=/t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" /p:DefineConstants="DEBUG;TRACE" /p:METABASE=prod /verbosity:normal /maxcpucount:1
+
+set BUILD_ARGS=/t:Restore;Rebuild /p:Configuration=Release /p:Platform="Any CPU" /p:DefineConstants="DEBUG;TRACE" /p:METABASE=prod /verbosity:normal /maxcpucount:1
 
 echo Building Release NFX ---------------------------------------
 echo todo: future include nuget pub
