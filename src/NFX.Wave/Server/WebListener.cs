50fffeddfdb6258ed997307daba3c2ee8c145e6b
Dmitriy Khmaladze <dmitriy@itadapter.com>
2018-03-23 20:21:57 -0400

    wip

---
+++ src/NFX.Wave/Server/WebListener.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NFX.ServiceModel;
+
+namespace NFX.Wave.Server
+{
+  /// <summary>
+  /// Provides base abstraction for web servers
+  /// </summary>
+  public abstract class WebListener : Service<WaveServer>
+  {
+    protected WebListener(WaveServer director) : base(director)
+    {
+
+    }
+
+    protected override void Destructor()
+    {
+      base.Destructor();
+    }
+
+
+  }
+}
---
+++ src/providers/NFX.Wave.Kestrel/KestrelWebListener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+using NFX;
+using NFX.Wave;
+using NFX.Wave.Server;
+
+namespace NFX.Wave.Kestrel
+{
+  public sealed class KestrelWebListener : WebListener
+  {
+    public KestrelWebListener(WaveServer waveServer) : base(waveServer)
+    {
+       m_Kestrel = new KestrelServer(null, null, null);
+    }
+
+    private KestrelServer m_Kestrel;
+
+  }
+}
