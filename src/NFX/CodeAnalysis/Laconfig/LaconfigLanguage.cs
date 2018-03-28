84185220dd2508a3338ee48332be3da6d53dd133
Dmitriy Khmaladze <dmitriy@itadapter.com>
2018-03-27 21:26:29 -0400

    Sync

--- src/NFX/CodeAnalysis/Laconfig/LaconfigLanguage.cs
+++ src/NFX/CodeAnalysis/Laconfig/LaconfigLanguage.cs
@@ -23,40 +23,37 @@ using NFX.CodeAnalysis.Source;
 
 namespace NFX.CodeAnalysis.Laconfig
 {
-    /// <summary>
-    /// Represents Laconic + Config = Laconfig terse configuration language
-    /// </summary>
-    public sealed class LaconfigLanguage : Language
+  /// <summary>
+  /// Represents Laconic + Config = Laconfig terse configuration language
+  /// </summary>
+  public sealed class LaconfigLanguage : Language
+  {
+    public static readonly LaconfigLanguage Instance = new LaconfigLanguage();
+
+    private LaconfigLanguage() : base() {}
+
+
+    public override LanguageFamily Family
     {
-        public static readonly LaconfigLanguage Instance = new LaconfigLanguage();
-
-        private LaconfigLanguage() : base() {}
-
-
-        public override LanguageFamily Family
-        {
-            get { return LanguageFamily.StructuredConfig; }
-        }
-
-        public override IEnumerable<string> FileExtensions
-        {
-            get
-            {
-                yield return "lac";
-                yield return "lacon";
-                yield return "laconf";
-                yield return "laconfig";
-                yield return "rschema";
-                yield return "amb";
-            }
-        }
-
-        public override ILexer MakeLexer(IAnalysisContext context, SourceCodeRef srcRef, ISourceText source, MessageList messages = null, bool throwErrors = false)
-        {
-            return new LaconfigLexer(context, srcRef, source, messages, throwErrors);
-        }
-
-
-
+        get { return LanguageFamily.StructuredConfig; }
     }
+
+    public override IEnumerable<string> FileExtensions
+    {
+        get
+        {
+            yield return "lac";
+            yield return "lacon";
+            yield return "laconf";
+            yield return "laconfig";
+            yield return "rschema";
+            yield return "amb";
+        }
+    }
+
+    public override ILexer MakeLexer(IAnalysisContext context, SourceCodeRef srcRef, ISourceText source, MessageList messages = null, bool throwErrors = false)
+    {
+        return new LaconfigLexer(context, srcRef, source, messages, throwErrors);
+    }
+  }
 }
