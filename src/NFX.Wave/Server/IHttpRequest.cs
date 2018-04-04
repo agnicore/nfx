0684cffde0bbc809996ee3ac3254ffe675c0d3c8
Dmitriy Khmaladze <dmitriy@itadapter.com>
2018-04-04 12:40:18 -0400

    stash

--- src/NFX.Wave/Server/IHttpRequest.cs
+++ src/NFX.Wave/Server/IHttpRequest.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace NFX.Wave.Server
 {
@@ -10,15 +12,31 @@ namespace NFX.Wave.Server
   /// </summary>
   public interface IHttpRequest
   {
-    string Scheme     { get; }
-    string Method     { get; }
+    string Scheme { get; }
+    string Method { get; }
+    IUri   URI    { get; }
 
-    IHttpHeaders  Headers { get;}
-    IHttpQuery    Query { get;}
+    IHttpHeaders  Headers { get; }
+    IHttpQuery    Query   { get; }
     IHttpCookies  Cookies { get; }
 
-    Stream Body { get; }
-    string ContentType { get; }
-    long?  ContentLength { get; }
+    Stream Body           { get; }
+    string ContentType    { get; }
+    long?  ContentLength  { get; }
+
+    /// <summary>
+    /// Returns true if the form was requested
+    /// </summary>
+    bool IsFormContent{ get; }
+
+    /// <summary>
+    /// Returns requested form or null
+    /// </summary>
+    IHttpForm  Form{ get;}
+
+    /// <summary>
+    /// Gets request body as a form or null
+    /// </summary>
+    Task<IHttpForm>  GetFormAsync(CancellationToken cancellationToken = default(CancellationToken));
   }
 }
